31ad0ff76116cc110c53e2829a036099566a3ec3
Caio Machado <caio.machado.dev@gmail.com>
2024-05-14 20:53:04 -0300

    modified

---
+++ js/acessibilidade.js
@@ -0,0 +1,26 @@
+// function changeFonteSize(e) {
+//   var elemento = $("h6, p");
+// 	var fonte = parseInt(elemento.css('font-size'));
+// 	var body = $("body");
+// 	const fonteNormal = parseInt(body.css('font-size'));
+
+
+// 	if (e == 'aumentar') {
+// 		fonte++;
+// 	}
+// 	if (e == 'diminuir'){
+// 		fonte--;
+// 	}
+// 	if (e == 'normal'){
+// 		fonte = fonteNormal;
+// 	}
+
+// 	elemento.css("fontSize", fonte);
+// }
+
+// // function contraste(){
+// //   $("body")
+// //       .css("background-color", "black")
+// //       .css("color", "white");
+// // }
+
