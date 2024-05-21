d9b261201462f560e667104f1a93591da9248399
José Cardoso <115185989+cardosojse@users.noreply.github.com>
2024-05-21 15:59:38 -0300

    Add files via upload

--- js/dam.js
+++ js/dam.js
@@ -22,3 +22,14 @@ cortina.addEventListener("click", ()=>{
     sidebar.classList.remove("show-sidebar");
     cortina.classList.remove("show-curtain");
 })
+
+document.addEventListener("DOMContentLoaded", function(){
+    const tributos = document.querySelectorAll(".tributos");
+
+    tributos.forEach(item => {
+        item.addEventListener("click", function(){
+            tributos.forEach(i => i.classList.remove('selected'));
+            this.classList.add('selected');
+        })
+    })
+});
